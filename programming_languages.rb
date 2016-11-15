338e45b0c2f33849ead2bf3c8e23d9e7ca488760
p sc <patty.stacruz@gmail.com>
2016-11-15 22:59:43 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
-  # your code here
+  newhash = {}
+
+  languages.values.each do |x|
+  	x.keys.each do |y|
+  		newhash[y] = {:type => "", :style => []}
+  	end
+  end
+
+  languages.values.each do |langtype|
+  	langtype.each do |k, v|
+  		newhash[k.to_sym][:type] = v.values[0]
+  	end
+  end
+
+  languages.each do |keystyle, keyval|
+  	keyval.each do |key, val|
+  		newhash[key.to_sym][:style] << keystyle
+  	end
+  end
+  return newhash
 end
